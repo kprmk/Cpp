e2f54b0c2c36fefbfcdccaa514dfb750dba52074
kprmk <kpriemko@gmail.com>
2019-10-20 00:01:37 +0300

    5-8 les_3

---
+++ lessons_I/5-8/les_3_condition_if_else_cycle_while/code/les_3_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Output:" << endl;
+    cout << (7 > 4) << endl;
+    if (7 > 4) 
+    {
+        cout <<  "Yes"; 
+    }
+    return (0);
+}
+
---
+++ lessons_I/5-8/les_3_condition_if_else_cycle_while/code/les_3_4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 1;
+    cout << "Start" << endl;
+    while (num < 6) 
+    {
+        cout << "Number: " << num << endl;
+        num++; // num = num + 1
+    }
+    cout << "It has finished" << endl;
+    return (0);
+}
+

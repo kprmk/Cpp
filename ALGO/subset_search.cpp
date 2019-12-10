346663f0872ebbdf1bc9f08015eea3ab0149e025
kprmk <kpriemko@gmail.com>
2019-12-10 20:15:30 +0300

    subset_search

---
+++ ALGO/subset_search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void    search(vector<int> subset, int n, int k, int *counter)
+{
+    if (k == n + 1)
+    {
+        *counter += 1;
+        printf("%d -> { ", *counter);
+        for (auto i: subset)
+            printf("%d ", i);
+        printf("}\n");
+    }
+    else
+    {
+        subset.push_back(k);
+        search(subset, n, k + 1, counter);
+        subset.pop_back();
+        search(subset, n, k + 1, counter);
+    }
+}
+
+int main()
+{    
+    vector<int> subset;
+    int n = 2;
+    int counter = 0;
+    search(subset, n, 1, &counter);
+
+    return (0);
+}
+
---
+++ individual/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char alpha = 'a' + 1;
+    alpha = alpha + 1;
+    cout << alpha << endl;
+    return (0);
+}
